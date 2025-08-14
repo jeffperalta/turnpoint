9b25522017c5f0a206d35c8b67b7ffd8b2ee0be1
Jeff Peralta <jaydocperalta@gmail.com>
2025-08-14 14:25:37 +1000

    UI: modification at LandingPage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { getSessionToken } from '../utility/SessionUtil';
 
 export default function LandingPage() {
+  const token = getSessionToken();
+
   return (
     <div className="box-container">
       <h1>Welcome to the Client Management App</h1>
-      <p>Use the navigation above to manage clients.</p>
+      {
+        token && 
+        <p>Use the navigation above to manage clients.</p>
+      }
+      {
+        !token && 
+        <p>Login to access the client information</p>
+      }
+      
     </div>
   );
 }
