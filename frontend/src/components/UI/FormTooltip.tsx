992c701eece3a68b986c04d6818f0bbc0f25acdd
Jeff Peralta <jaydocperalta@gmail.com>
2025-09-24 23:05:30 +1000

    Tooltip

--- frontend/src/components/UI/FormTooltip.tsx
+++ frontend/src/components/UI/FormTooltip.tsx
@@ -1,5 +1,5 @@
 import { Tooltip, IconButton } from "@mui/material";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+// import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import './FormTooltip.css';
 
 export default function FormTooltip({
@@ -9,7 +9,7 @@ export default function FormTooltip({
   return (
     <Tooltip title={message} className="form-tooltip">
       <IconButton size="small" aria-label="info" sx={{ padding: 0 }}>
-        <InfoOutlinedIcon fontSize="small" htmlColor="#1976d2"/>
+        {/* <InfoOutlinedIcon fontSize="small" htmlColor="#1976d2"/> */}
       </IconButton>
     </Tooltip>
   )
